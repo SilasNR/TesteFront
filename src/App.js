d937970296c88f45d1860063035937ee0e259be8
Silas Rodrigues <silasnas2018@gmail.com>
2025-05-27 15:26:16 -0300

    teste navegação

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ProdutoList from './components/ProdutoList';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Bem-vindo ao Sistema de Produtos</h1>
+      <NavBar />
       <ProdutoList />
     </div>
   );
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './NavBar.css'
+
+function NavBar (){
+
+
+    return(
+        <>
+            <div id="Barra">
+                <h1>Sistema</h1>
+            </div>
+        </>
+    )
+}
+
+export default NavBar;
